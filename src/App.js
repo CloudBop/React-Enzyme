d318c7c42bb8e11f4344e8dafcc0df4238d12914
Cloudbop <boppincolin@gmail.com>
2021-07-08 23:31:01 +0100

    +pass - clicking btn increments counter display

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [counter, setCounter] = useState(0)
   return (
     <div
       className="App"
       data-test="component-app"
     >
-      <h1 data-test="counter-display">The counter is currently, <span data-test="count" >0</span> </h1>
-      <button data-test="increment-btn">increment</button>
+      <h1 data-test="counter-display">The counter is currently, <span data-test="count" >{counter}</span> </h1>
+      <button onClick={() => setCounter(prev => prev + 1)} data-test="increment-btn">increment</button>
 
     </div>
   )

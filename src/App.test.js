5a6d2594b0d24565809708bf726ad658cc7670dd
Cloudbop <boppincolin@gmail.com>
2021-07-08 23:19:30 +0100

    refactored tests with helpers

--- src/App.test.js
+++ src/App.test.js
@@ -3,22 +3,25 @@ import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17'
 import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+const setup = () => shallow(<App />)
+const findByTestAttribute = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
 test('renders without error', () => {
   //
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const wrapper = setup()
+  const appComponent = findByTestAttribute(wrapper, 'component-app');
   expect(appComponent.length).toBe(1);
 
 });
 test('renders increment button', () => {
   //
-  const wrapper = shallow(<App />);
-  const button = wrapper.find("[data-test='increment-btn']");
+  const wrapper = setup()
+  const button = findByTestAttribute(wrapper, 'increment-btn');
   expect(button.length).toBe(1);
 })
 test('renders counter display', () => {
-  const wrapper = shallow(<App />);
-  const display = wrapper.find("[data-test='counter-display']");
+  const wrapper = setup()
+  const display = findByTestAttribute(wrapper, 'counter-display');
   expect(display.length).toBe(1);
 })
 test('inits counter at 0', () => {
